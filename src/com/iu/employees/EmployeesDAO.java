57d82e62ef9afcc199378befeea4749ddac2e4f7
Namjin-kimm <namjin-kim@daum.net>
2022-07-27 17:49:57 +0900

    employees

---
+++ src/com/iu/employees/EmployeesDAO.java
@@ -0,0 +1,51 @@
+package com.iu.employees;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.iu.util.DBConnector;
+
+public class EmployeesDAO {
+	
+	public EmployeesDTO getDetail(int employee_id) throws Exception {
+		
+	//1. employees에서 하나의 결과
+	
+			//1. DB 연결
+		Connection con = DBConnector.getConnection();
+			
+			//2. SQL문 작성
+		String sql = "select * from employees where employee_id = ?";
+			
+			//변수가 들어갈 자리는 ? 로 표시한다
+			
+			//3. 미리 전송
+		PreparedStatement st = con.prepareStatement(sql);
+			
+			//4. ? 값 세팅
+		st.setInt(1, employee_id);
+			//where num between?(1) and ?(2)
+			
+			
+			//5. 최종 전송 후 결과 처리
+		ResultSet rs = st.executeQuery();
+		EmployeesDTO employeesDTO = new EmployeesDTO();
+		employeesDTO.setEmployee_id(rs.getInt(1));
+		employeesDTO.setFirst_name(rs.getString(2));
+		employeesDTO.setLast_name(rs.getString(3));
+		employeesDTO.setEmail(rs.getString(4));
+		employeesDTO.setPhone_number(rs.getString(5));
+		employeesDTO.setHire_date(rs.getDate(6));
+		employeesDTO.setJob_id(rs.getString(7));
+		employeesDTO.setSalary(rs.getInt(8));
+		employeesDTO.setCommission_pct(rs.getDouble(9));
+		employeesDTO.setManager_id(rs.getInt(10));
+		employeesDTO.setDepartment_id(rs.getInt(11));
+		
+		
+			
+		return employeesDTO;
+	}
+
+}
